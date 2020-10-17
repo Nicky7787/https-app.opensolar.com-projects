6f3f603faea68aa8d64a7423d6b7e89995f0aab3
Melissa Nicole Benford <56089692+Nicky7787@users.noreply.github.com>
2020-10-16 20:57:57 -0500

    Initialize roblou/fileSearchChanges

---
+++ src/vs/editor/common/services/modelService.ts
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Event } from 'vs/base/common/event';
+import { URI } from 'vs/base/common/uri';
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import { ITextModel, ITextModelCreationOptions, ITextBufferFactory } from 'vs/editor/common/model';
+import { IMode } from 'vs/editor/common/modes';
+
+export const IModelService = createDecorator<IModelService>('modelService');
+
+export interface IModelService {
+	_serviceBrand: any;
+
+	createModel(value: string | ITextBufferFactory, modeOrPromise: Promise<IMode> | IMode, resource: URI | undefined, isForSimpleWidget?: boolean): ITextModel;
+
+	updateModel(model: ITextModel, value: string | ITextBufferFactory): void;
+
+	setMode(model: ITextModel, modeOrPromise: Promise<IMode> | IMode): void;
+
+	destroyModel(resource: URI): void;
+
+	getModels(): ITextModel[];
+
+	getCreationOptions(language: string, resource: URI, isForSimpleWidget: boolean): ITextModelCreationOptions;
+
+	getModel(resource: URI): ITextModel | null;
+
+	onModelAdded: Event<ITextModel>;
+
+	onModelRemoved: Event<ITextModel>;
+
+	onModelModeChanged: Event<{ model: ITextModel; oldModeId: string; }>;
+}
+
+export function shouldSynchronizeModel(model: ITextModel): boolean {
+	return (
+		!model.isTooLargeForSyncing() && !model.isForSimpleWidget
+	);
+}
