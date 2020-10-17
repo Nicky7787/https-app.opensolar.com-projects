9bb1ee2decb7c71264b0e22ae0aca262ef849843
Melissa Nicole Benford <56089692+Nicky7787@users.noreply.github.com>
2020-10-16 20:45:00 -0500

    Initialize roblou/searchAPIchanges

---
+++ extensions/typescript-language-features/src/features/documentHighlight.ts
@@ -0,0 +1,59 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import * as Proto from '../protocol';
+import { ITypeScriptServiceClient } from '../typescriptService';
+import * as typeConverters from '../utils/typeConverters';
+
+
+
+class TypeScriptDocumentHighlightProvider implements vscode.DocumentHighlightProvider {
+	public constructor(
+		private readonly client: ITypeScriptServiceClient
+	) { }
+
+	public async provideDocumentHighlights(
+		resource: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken
+	): Promise<vscode.DocumentHighlight[]> {
+		const file = this.client.toPath(resource.uri);
+		if (!file) {
+			return [];
+		}
+
+		const args = typeConverters.Position.toFileLocationRequestArgs(file, position);
+		let items: Proto.OccurrencesResponseItem[] | undefined;
+		try {
+			const response = await this.client.execute('occurrences', args, token);
+			items = response.body;
+		} catch {
+			// noop
+		}
+
+		if (!items) {
+			return [];
+		}
+
+		return items
+			.filter(x => !x.isInString)
+			.map(documentHighlightFromOccurance);
+	}
+}
+
+function documentHighlightFromOccurance(occurrence: Proto.OccurrencesResponseItem): vscode.DocumentHighlight {
+	return new vscode.DocumentHighlight(
+		typeConverters.Range.fromTextSpan(occurrence),
+		occurrence.isWriteAccess ? vscode.DocumentHighlightKind.Write : vscode.DocumentHighlightKind.Read);
+}
+
+export function register(
+	selector: vscode.DocumentSelector,
+	client: ITypeScriptServiceClient,
+) {
+	return vscode.languages.registerDocumentHighlightProvider(selector,
+		new TypeScriptDocumentHighlightProvider(client));
+}
