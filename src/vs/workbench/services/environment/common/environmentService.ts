7bd4a9aa636d7de7a9abd8e3ed99d79bd052ee6b
Melissa Nicole Benford <56089692+Nicky7787@users.noreply.github.com>
2020-10-16 22:20:26 -0500

    Initialize roblou/fixExportTask

---
+++ src/vs/workbench/services/environment/common/environmentService.ts
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { createDecorator, ServiceIdentifier } from 'vs/platform/instantiation/common/instantiation';
+import { IWindowConfiguration } from 'vs/platform/windows/common/windows';
+import { IEnvironmentService } from 'vs/platform/environment/common/environment';
+import { IWorkbenchConstructionOptions } from 'vs/workbench/workbench.web.api';
+import { URI } from 'vs/base/common/uri';
+
+export const IWorkbenchEnvironmentService = createDecorator<IWorkbenchEnvironmentService>('environmentService');
+
+export interface IWorkbenchEnvironmentService extends IEnvironmentService {
+
+	_serviceBrand: ServiceIdentifier<IEnvironmentService>;
+
+	readonly configuration: IWindowConfiguration;
+
+	readonly logFile: URI;
+
+	readonly options?: IWorkbenchConstructionOptions;
+}
